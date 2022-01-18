fe42a996871c774dc98dda0521a2cdf5eefbcc24
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2022-01-18 12:20:08 -0300

    añadido user

--- src/modules/tuits/dto/create-tuit.dto.ts
+++ src/modules/tuits/dto/create-tuit.dto.ts
@@ -1,6 +1,11 @@
-import { IsString } from "class-validator";
+import { IsObject, IsString } from 'class-validator';
+
+import { User } from '../../users/entities/user.entity';
 
 export class CreateTuitDto {
-    @IsString()
-    readonly message: string;
+  @IsString()
+  readonly message: string;
+
+  @IsObject()
+  readonly user: Partial<User>;
 }
