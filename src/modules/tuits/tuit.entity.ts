647d43d51b49e58a9dad4465b0dbace02d89528d
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2022-01-18 12:19:04 -0300

    pagination dto

--- src/modules/tuits/tuit.entity.ts
+++ src/modules/tuits/tuit.entity.ts
@@ -1,4 +1,22 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { User } from '../users/entities';
+
+@Entity()
 export class Tuit {
-    id: string;
-    message: string;
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  message: string;
+
+  @ManyToOne((type) => User, (user) => user.tuits, { cascade: true })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
 }
